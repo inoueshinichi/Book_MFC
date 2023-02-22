dba1e1e6718b576bd9aabf68cd5b59c1bafe8cb0
InoueShinichi XPS15 Windows <inoue.shinichi.1800@gmail.com>
2023-02-22 15:19:57 +0900

    [Update] new sample

---
+++ SampleThread/SampleThread.h
@@ -0,0 +1,32 @@
+﻿
+// SampleThread.h : PROJECT_NAME アプリケーションのメイン ヘッダー ファイルです
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH に対してこのファイルをインクルードする前に 'pch.h' をインクルードしてください"
+#endif
+
+#include "resource.h"		// メイン シンボル
+
+
+// CSampleThreadApp:
+// このクラスの実装については、SampleThread.cpp を参照してください
+//
+
+class CSampleThreadApp : public CWinApp
+{
+public:
+	CSampleThreadApp();
+
+// オーバーライド
+public:
+	virtual BOOL InitInstance();
+
+// 実装
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CSampleThreadApp theApp;
---
+++ SampleThread/SampleThreadDlg.h
@@ -0,0 +1,34 @@
+﻿
+// SampleThreadDlg.h : ヘッダー ファイル
+//
+
+#pragma once
+
+
+// CSampleThreadDlg ダイアログ
+class CSampleThreadDlg : public CDialogEx
+{
+// コンストラクション
+public:
+	CSampleThreadDlg(CWnd* pParent = nullptr);	// 標準コンストラクター
+
+// ダイアログ データ
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_SAMPLETHREAD_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV サポート
+
+
+// 実装
+protected:
+	HICON m_hIcon;
+
+	// 生成された、メッセージ割り当て関数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
